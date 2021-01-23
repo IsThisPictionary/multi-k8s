325e90e1cfe096ca0828b70b1853eedffb4fb597
unknown <tranbaanthony@gmail.com>
2021-01-23 18:59:38 +0900

    changed header

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
           >
             Learn React
           </a>
+          <h>Fib calculator</h>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
